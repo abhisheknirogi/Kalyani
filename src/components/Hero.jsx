93ef849c1b3704268a21b37be6fb288a24fc6a04
abhisheknirogi <abhishek27127@gmail.com>
2026-01-24 17:12:04 +0530

    hello

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ export default function Hero() {
     <section className="hero">
       <div className="container">
         <h1>Blooms and Bloosom</h1>
-        <p>Draping grace, one pleat at a time</p>
+        <p>Beauty, one brush stroke at a time.</p>
       </div>
     </section>
   );
